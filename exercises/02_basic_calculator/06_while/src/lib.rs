27c37b15708cc1646c3ff6f1970dc49da53f6a3f
apv1985 <prados_vilchez@hotmail.com>
2024-10-27 22:17:56 +0000

    more exercises completed

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,16 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut target = n;
+    let mut fact = 1;
+
+    while target > 0
+    {
+        fact = fact * target;
+        target = target - 1;
+    }
+
+    fact
 }
 
 #[cfg(test)]
